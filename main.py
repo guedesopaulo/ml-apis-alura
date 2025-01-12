9790484b035ed94590c70754c97d2f96e061e00c
guedesopaulo <guedesopaulo@gmail.com>
2025-01-12 15:37:52 -0300

    feat: Basic Auth

--- main.py
+++ main.py
@@ -3,22 +3,27 @@ import pickle
 
 from flask import Flask
 from flask import jsonify
+from flask_basicauth import BasicAuth
 from flask import request
 from googletrans import Translator
 from textblob import TextBlob
-
 
 model = pickle.load(open('modelo.sav', 'rb'))
 colunas = ['tamanho', 'ano', 'garagem']
 
 
 app = Flask(__name__)
+app.config['BASIC_AUTH_USERNAME'] = 'paulo'
+app.config['BASIC_AUTH_PASSWORD'] = 'alura'
+
+basic_auth = BasicAuth(app)
 
 @app.route('/')
 def home():
     return "Minha Primeira API."
 
 @app.route('/sentimento/<frase>')
+@basic_auth.required
 def sentimento(frase):
     translator = Translator()
     traducao = translator.translate(frase, src='pt', dest='en')
