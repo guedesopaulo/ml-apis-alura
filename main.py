a3279d927c52a71e562cd75aa57701401f7a6e2a
guedesopaulo <guedesopaulo@gmail.com>
2025-01-12 15:18:53 -0300

    feat: First house prices API

--- main.py
+++ main.py
@@ -1,6 +1,20 @@
+import pandas as pd
+
 from flask import Flask
-from textblob import TextBlob
 from googletrans import Translator
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from textblob import TextBlob
+
+
+df = pd.read_csv("casas.csv")
+colunas = ['tamanho', 'preco']
+df = df[colunas]
+X = df.drop('preco', axis=1)
+y = df['preco']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+model= LinearRegression()
+model.fit(X_train, y_train)
 
 app = Flask(__name__)
 
@@ -16,5 +30,10 @@ def sentimento(frase):
     polatidade = tb.sentiment.polarity
     return f"polatidade {polatidade}"
 
+
+@app.route('/cotacao/<int:tamanho>')
+def cotacao(tamanho):
+    preco = model.predict([[tamanho]])
+    return str(preco)
 
 app.run(debug=True)
