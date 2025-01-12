94af4d55837aa3f1e600aa0e2f9fc3ee421a792b
guedesopaulo <guedesopaulo@gmail.com>
2025-01-12 14:54:36 -0300

    feat: Sentiment analisys in API

--- main.py
+++ main.py
@@ -1,9 +1,20 @@
 from flask import Flask
+from textblob import TextBlob
+from googletrans import Translator
 
-app = Flask('meu_app')
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return "Minha Primeira API."
 
-app.run()
+@app.route('/sentimento/<frase>')
+def sentimento(frase):
+    translator = Translator()
+    traducao = translator.translate(frase, src='pt', dest='en')
+    tb = TextBlob(traducao.text)
+    polatidade = tb.sentiment.polarity
+    return f"polatidade {polatidade}"
+
+
+app.run(debug=True)
